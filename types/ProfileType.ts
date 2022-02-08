b2d574b0941a21c096e19084a781e9cd12e57f9b
Juanlu <juanlu@wuolah.com>
2022-02-08 11:47:20 +0100

    minor changes

--- types/ProfileType.ts
+++ types/ProfileType.ts
@@ -17,8 +17,8 @@ export type ProfileCommunityType = z.infer<typeof ProfileCommunitySchema>;
 export const ProfileSchema = z.object({
   id: z.number().positive(),
   nickname: z.string(),
-  popularity: z.number(),
-  money: z.number().positive(),
+  popularity: z.number().nonnegative().default(0),
+  money: z.number().nonnegative().default(0),
 });
 
 export type ProfileType = z.infer<typeof ProfileSchema>;
