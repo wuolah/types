f702c0f261a4d0a5ed6972ca7300591b247c1d90
FernandoValdes <fernando.valdes@wuolah.com>
2022-02-11 12:13:03 +0100

    Add user to ProfileCommunity

--- types/ProfileType.ts
+++ types/ProfileType.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 import { CommunitySchema } from "..";
 
+export const ProfileSchema = z.object({
+  id: z.number().positive(),
+  nickname: z.string(),
+  popularity: z.number().nonnegative().default(0),
+  money: z.number().nonnegative().default(0),
+});
+
+export type ProfileType = z.infer<typeof ProfileSchema>;
+
 export const ProfileCommunitySchema = z.object({
   userId: z.number().positive(),
   communityId: z.number().positive(),
@@ -10,15 +19,7 @@ export const ProfileCommunitySchema = z.object({
   updatedAt: z.date(),
 
   community: CommunitySchema.optional(),
+  user: ProfileSchema.optional(),
 });
 
 export type ProfileCommunityType = z.infer<typeof ProfileCommunitySchema>;
-
-export const ProfileSchema = z.object({
-  id: z.number().positive(),
-  nickname: z.string(),
-  popularity: z.number().nonnegative().default(0),
-  money: z.number().nonnegative().default(0),
-});
-
-export type ProfileType = z.infer<typeof ProfileSchema>;
