6296292f4b3613396cafe30c7ebed0d0b53a0e4d
FernandoValdes <fernando.valdes@wuolah.com>
2022-04-12 10:07:11 +0200

    sync communityB

--- types/Community.ts
+++ types/Community.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
 import { CenterSchema } from "./Center";
+import { ProfileSchema } from "./Profile";
 import { StudySchema } from "./Study";
-import { UniversitySchema } from "./University";
+
+export const CommunityStatus = {
+  DEFAULT: "DEFAULT",
+  TO_CREATE: "TO_CREATE",
+  TO_REVIEW: "TO_REVIEW",
+  COMPLETED: "COMPLETED",
+} as const;
+export const CommunityStatusSchema = z.nativeEnum(CommunityStatus);
+export type CommunityStatusType = z.infer<typeof CommunityStatusSchema>;
 
 export const CommunitySchema = z.object({
   id: z.number().positive(),
@@ -10,17 +19,33 @@ export const CommunitySchema = z.object({
   shortName: z.string().optional(),
   logoUrl: z.string().optional(),
   backgroundUrl: z.string().optional(),
-  universityId: z.number().nonnegative(),
   centerId: z.number().nonnegative(),
   studyId: z.number().nonnegative(),
   numUsers: z.number().nonnegative().default(0),
   createdAt: z.string(),
   updatedAt: z.string(),
+  type: z.number().optional(),
+  courses: z.number().nonnegative().optional(),
+  enabled: z.number().optional(), //Union Boolean
+  createdBy: z.number().nonnegative().optional(),
+  blocked: z.number().optional(), //Union Boolean
+  blockedBy: z.number().nonnegative().optional(),
+  status: CommunityStatusSchema.optional(),
+  completedDate: z.date().optional(),
+  studyPlan: z.string().optional(),
+  collaboratorIds: z.array(z.number()).optional(),
+  completedCheckAdminId: z.number().nonnegative().optional(),
+  processId: z.number().nonnegative().optional(),
+  informantId: z.number().nonnegative().optional(),
+  verified: z.number().optional(), //Union Boolean
+  deleted: z.number().optional(), //Union Boolean
+  deletedAt: z.date().optional(),
 
-  university: UniversitySchema.optional(),
   center: CenterSchema.optional(),
   study: StudySchema.optional(),
-
+  informant: ProfileSchema.optional(),
+  collaborators: z.array(ProfileSchema).optional(),
+  admin: ProfileSchema.optional(),
   // admins?: User[];
   // collaborators?: User[];
 });
