54c9e2c02cd12d2ba7712905654e35aa8bbdd8f6
Juanlu <juanludlf@gmail.com>
2022-04-26 14:53:20 +0200

    feat: added center.universityId

--- types/Center.ts
+++ types/Center.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
+import { UniversitySchema } from "./University";
 
 export const CenterSchema = z.object({
   id: z.number().nonnegative(),
   slug: z.string(),
   name: z.string(),
   shortName: z.string().nullable().optional(),
+  universityId: z.number().nonnegative(),
   logoUrl: z.string().nullable().optional(),
+
+  // virtuals
+  university: UniversitySchema.optional(),
 });
 
 export type CenterType = z.infer<typeof CenterSchema>;
