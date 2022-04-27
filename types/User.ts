93f691fa36eb40a181a956cc7140bc0371997b2e
Fernando <fernando.valdes@wuolah.com>
2022-04-27 10:39:58 +0200

    added lastConnection to userSchema

--- types/User.ts
+++ types/User.ts
@@ -14,6 +14,7 @@ export const UserSchema = ProfileSchema.extend({
   password: z.string().optional(),
   coins: z.number().nonnegative().optional(),
   captchaCounter: z.number().nonnegative().default(0),
+  lastConnection: z.string()
 });
 
 export type UserType = z.infer<typeof UserSchema>;
