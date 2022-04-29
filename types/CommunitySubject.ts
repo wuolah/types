d0b927d87292320f1430e411a1f387b2e40e0a73
Fernando <fernando.valdes@wuolah.com>
2022-04-29 14:26:32 +0200

    refactor subjects

--- types/CommunitySubject.ts
+++ types/CommunitySubject.ts
@@ -1,14 +1,11 @@
 import { z } from "zod";
-import { CommunitySchema } from "./Community";
-import { SubjectSchema } from "./Subject";
 import { stringToBoolean } from "./utils";
 
 export const CommunitySubjectSchema = z.object({
   id: z.number().positive(),
   centerId: z.number().nonnegative(),
   studyId: z.number().nonnegative(),
-  subjectId: z.number().nonnegative(),
-  course: z.number().positive(),
+  course: z.number().nonnegative(),
   numFiles: z.number().nonnegative().default(0).optional(),
   verified: z
     .preprocess(stringToBoolean(null), z.boolean().nullable())
@@ -18,9 +15,6 @@ export const CommunitySubjectSchema = z.object({
     .optional(),
   createdAt: z.string(),
   updatedAt: z.string(),
-
-  community: CommunitySchema.optional(),
-  subject: SubjectSchema.optional(),
 });
 
 export type CommunitySubjectType = z.infer<typeof CommunitySubjectSchema>;
--- types/Subject.ts
+++ types/Subject.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { CommunitySubjectSchema } from "./CommunitySubject";
 import { stringToBoolean } from "./utils";
 
 export const SubjectSchema = z.object({
@@ -6,14 +7,15 @@ export const SubjectSchema = z.object({
   slug: z.string(),
   name: z.string(),
   shortName: z.string().nullable().optional(),
-  course: z.number().nonnegative().optional(),
-  numFiles: z.number().nonnegative().default(0).optional(),
   verified: z
     .preprocess(stringToBoolean(true), z.boolean().nullable())
     .optional(),
   enabled: z
     .preprocess(stringToBoolean(true), z.boolean().nullable())
     .optional(),
+  relation: CommunitySubjectSchema.optional(),
 });
+
+
 
 export type SubjectType = z.infer<typeof SubjectSchema>;
