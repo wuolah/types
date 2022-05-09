abc2bba98f9d0d771c7bed7b8a8fc260886a6fe3
Fernando <fernando.valdes@wuolah.com>
2022-05-09 13:37:28 +0200

    add createdAt to BookmarkUser

--- types/BookmarkUser.ts
+++ types/BookmarkUser.ts
@@ -6,9 +6,10 @@ export const BookmarkUserSchema = z.object({
   id: z.number().nonnegative(),
   userId: z.number().nonnegative(),
   profileId: z.number().nonnegative(),
+  createdAt: z.string(),
 
   user: ProfileSchema.optional(),
   profile: ProfileSchema.optional(),
-})
+});
 
 export type BookmarkUserType = z.infer<typeof BookmarkUserSchema>;
