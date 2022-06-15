62ccc8ac27b4414f15a3134e7af74032dd8b94c3
Ivan Coronado Moreno <ivacormor@gmail.com>
2022-06-15 08:46:41 +0200

    feat(Artifact): add discriminatedUnion

--- types/Artifact.ts
+++ types/Artifact.ts
@@ -1,16 +1,39 @@
 import { z } from "zod";
+import { DocumentSchema } from "./Document";
+import { SocialSchema } from "./Social";
 
 export const ArtifactTypeEnum = {
-  DOCUMENTS: "DOCUMENTS",
+  DOCUMENT: "DOCUMENT",
   SOCIAL: "SOCIAL",
   FOLDER: "FOLDER",
 } as const;
 export const ArtifactTypeSchema = z.nativeEnum(ArtifactTypeEnum);
 export type ArtifactTypeType = z.infer<typeof ArtifactTypeSchema>;
 
-export const ArtifactSchema = z.object({
+const BaseArtifact = z.object({
   id: z.string(),
-  type: ArtifactTypeSchema,
+  name: z.string(),
 });
+
+const DocumentArtifact = BaseArtifact.extend({
+  type: z.literal(ArtifactTypeEnum.DOCUMENT),
+  value: DocumentSchema,
+});
+
+const FolderArtifact = BaseArtifact.extend({
+  type: z.literal(ArtifactTypeEnum.FOLDER),
+  value: z.object({}),
+});
+
+const SocialArtifact = BaseArtifact.extend({
+  type: z.literal(ArtifactTypeEnum.SOCIAL),
+  value: SocialSchema,
+});
+
+export const ArtifactSchema = z.discriminatedUnion("type", [
+  DocumentArtifact,
+  FolderArtifact,
+  SocialArtifact,
+]);
 
 export type ArtifactType = z.infer<typeof ArtifactSchema>;
